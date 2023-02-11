6a2ecd4f39d9f2a1d009433595e555f300a006d0
Ahmed Uddin Siddiqui <aus1st@gmail.com>
2023-02-11 21:32:26 +0500

    layout-formatting

--- next-js-steps/app/layout.tsx
+++ next-js-steps/app/layout.tsx
@@ -20,21 +20,21 @@ export default function RootLayout({
       <head />
       <body>
         <div className={styles.container}>
-        <header className={styles.header}>
-        <Link href="/">
-        <Image
-          priority
-          src="/images/profile.jpg"
-          className={utilStyles.borderCircle}
-          height={108}
-          width={108}
-          alt=""/>
-       </Link> 
-      <h1 className={styles.heading2Xl}>{name}</h1>
-      {children}
-        </header>
+          <header className={styles.header}>
+            <Link href="/">
+              <Image
+                priority
+                src="/images/profile.jpg"
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt="" />
+            </Link>
+            <h1 className={styles.heading2Xl}>{name}</h1>
+            {children}
+          </header>
         </div>
-        </body>
+      </body>
     </html>
   )
 }
